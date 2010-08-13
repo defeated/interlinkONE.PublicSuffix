d91b7d6246ba3261616a25da68ca6ea30b1ab86e
defeated <defeated2k4@gmail.com>
2010-08-13 13:58:28 -0400

    worked on Domain specifications

---
+++ specs/PublicSuffix.Specs/DomainSpecs.cs
@@ -0,0 +1,75 @@
+﻿
+using Machine.Specifications;
+
+namespace PublicSuffix.Specs {
+
+    public class when_given_a_tld {
+        static Domain domain;
+
+        Establish context = () => domain = new Domain("com");
+
+        It has_a_tld            = () => domain.TLD.ShouldEqual("com");
+        It converts_to_a_string = () => domain.ToString().ShouldEqual("com");
+    }
+
+    public class when_given_a_maindomain {
+        static Domain domain;
+
+        Establish context = () => domain = new Domain("com", "google");
+
+        It has_a_tld            = () => domain.TLD.ShouldEqual("com");
+        It has_a_domain         = () => domain.MainDomain.ShouldEqual("google");
+        It converts_to_a_string = () => domain.ToString().ShouldEqual("google.com");
+    }
+
+    public class when_given_a_subdomain {
+        static Domain domain;
+
+        Establish context = () => domain = new Domain("com", "google", "maps");
+
+        It has_a_tld            = () => domain.TLD.ShouldEqual("com");
+        It has_a_domain         = () => domain.MainDomain.ShouldEqual("google");
+        It has_a_subdomain      = () => domain.SubDomain.ShouldEqual("maps");
+        It converts_to_a_string = () => domain.ToString().ShouldEqual("maps.google.com");
+    }
+
+    public class when_subdomain_converts_to_array {
+        static Domain domain;
+        static string[] parts;
+
+        Establish context = () => domain = new Domain("com", "google", "maps");
+        
+        Because of = () => parts = domain.ToArray();
+
+        It only_has_three_items = () => parts.Length.ShouldEqual(3);
+        It has_a_subdomain      = () => parts[0].ShouldEqual("maps");
+        It has_a_domain         = () => parts[1].ShouldEqual("google");
+        It has_a_tld            = () => parts[2].ShouldEqual("com");
+    }
+
+    public class when_domain_converts_to_array {
+        static Domain domain;
+        static string[] parts;
+
+        Establish context = () => domain = new Domain("com", "google");
+
+        Because of = () => parts = domain.ToArray();
+
+        It only_has_two_items   = () => parts.Length.ShouldEqual(2);
+        It has_a_domain         = () => parts[0].ShouldEqual("google");
+        It has_a_tld            = () => parts[1].ShouldEqual("com");
+    }
+
+    public class when_tld_converts_to_array {
+        static Domain domain;
+        static string[] parts;
+
+        Establish context = () => domain = new Domain("com");
+
+        Because of = () => parts = domain.ToArray();
+
+        It only_has_one_item    = () => parts.Length.ShouldEqual(1);
+        It has_a_tld            = () => parts[0].ShouldEqual("com");
+    }
+
+}
---
+++ src/PublicSuffix/Domain.cs
@@ -0,0 +1,34 @@
+﻿
+using System.Linq;
+
+namespace PublicSuffix {
+
+    public class Domain {
+
+        public string TLD { get; set; }
+        public string MainDomain { get; set; }
+        public string SubDomain { get; set; }
+
+        public Domain() : this("") {}
+        public Domain(string tld) : this(tld, "") {}
+        public Domain(string tld, string mainDomain) : this(tld, mainDomain, "") {}
+
+        public Domain(params string[] parts) {
+            this.TLD        = parts[0] ?? "";
+            this.MainDomain = parts[1] ?? "";
+            this.SubDomain  = parts[2] ?? "";
+        }
+
+        public override string ToString() {
+            return string.Join(".", this.ToArray());
+        }
+
+        public string[] ToArray() {
+            var array = new[] { this.SubDomain, this.MainDomain, this.TLD };
+            array = array.SkipWhile(string.IsNullOrEmpty).ToArray();
+            
+            return array;
+        }
+
+    }
+}
