604c43c8baf7f11d10e085c9bb44ecfe4d1d9921
defeated <defeated2k4@gmail.com>
2010-08-13 14:46:44 -0400

    worked on specs for Rules List

--- specs/PublicSuffix.Specs/RulesListSpecs.cs
+++ specs/PublicSuffix.Specs/RulesListSpecs.cs
@@ -1,35 +1,22 @@
 ﻿
+using System.Linq;
+
 using Machine.Specifications;
 
 namespace PublicSuffix.Specs {
 
-    public class when_given_a_text_file {
-
-        static string file;
+    public class when_rules_come_from_text_file {
+        static RulesList list;
         static string[] rules;
 
-        Establish context = () => {
-            file = @"data\effective_tld_names.dat";
-        };
+        Establish context = () => list = new RulesList();
 
-        Because of = () => {
-            var list = new RulesList();
-            rules = list.FromFile(file);
-        };
+        Because of = () => rules = list.FromFile(@"data\effective_tld_names.dat");
 
-        It removes_invalid_lines = () => {
-            rules.ShouldNotContain("");
-            rules.ShouldEachConformTo((line) => !line.StartsWith("//"));
-        };
-
-        It returns_an_array_of_rules = () => {
-            rules.Length.ShouldBeGreaterThan(0);
-        };
-
-        It is_in_the_correct_order = () => {
-            rules[0].ShouldEqual("ac");
-            rules[rules.Length - 1].ShouldEqual("*.zw");
-        };
+        It returns_an_array_of_rules    = () => rules.Length.ShouldBeGreaterThan(0);
+        It has_no_blank_lines           = () => rules.ShouldNotContain("");
+        It has_no_commented_lines       = () => rules.ShouldEachConformTo((line) => !line.StartsWith("//"));
+        It has_first_line               = () => rules.First().ShouldEqual("ac");
+        It has_last_line                = () => rules.Last().ShouldEqual("*.zw");
     }
-
 }
