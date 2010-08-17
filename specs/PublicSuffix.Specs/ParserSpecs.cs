3d376ec55b0fa776ee5423fa9f0a773439a91ecb
defeated <defeated2k4@gmail.com>
2010-08-17 18:00:18 -0400

    improved coverage of Parser specs

--- specs/PublicSuffix.Specs/ParserSpecs.cs
+++ specs/PublicSuffix.Specs/ParserSpecs.cs
@@ -24,6 +24,33 @@ namespace PublicSuffix.Specs {
     }
 
     [Subject(typeof(Parser))]
+    public class when_given_a_valid_url_with_no_subdomain : WithParser {
+        Establish context = () => domain = parser.Parse("http://google.com");
+
+        It parses_the_tld = () => domain.TLD.ShouldEqual("com");
+        It parses_the_maindomain = () => domain.MainDomain.ShouldEqual("google");
+        It parses_the_subdomain = () => domain.SubDomain.ShouldEqual("");
+    }
+
+    [Subject(typeof(Parser))]
+    public class when_given_a_valid_url_with_multiple_tld : WithParser {
+        Establish context = () => domain = parser.Parse("http://www.google.com.cn");
+
+        It parses_the_tld = () => domain.TLD.ShouldEqual("com.cn");
+        It parses_the_maindomain = () => domain.MainDomain.ShouldEqual("google");
+        It parses_the_subdomain = () => domain.SubDomain.ShouldEqual("www");
+    }
+
+    [Subject(typeof(Parser))]
+    public class when_given_a_valid_url_with_multiple_tld_and_multiple_subdomains : WithParser {
+        Establish context = () => domain = parser.Parse("http://www.maps.google.com.cn");
+
+        It parses_the_tld = () => domain.TLD.ShouldEqual("com.cn");
+        It parses_the_maindomain = () => domain.MainDomain.ShouldEqual("google");
+        It parses_the_subdomain = () => domain.SubDomain.ShouldEqual("www.maps");
+    }
+
+    [Subject(typeof(Parser))]
     public class when_given_an_invalid_url : WithParser {
         Establish context = () => domain = parser.Parse("http://fake.zzz");
 
@@ -36,6 +63,15 @@ namespace PublicSuffix.Specs {
 
         It parses_the_tld           = () => domain.TLD.ShouldEqual("co.uk");
         It parses_the_maindomain    = () => domain.MainDomain.ShouldEqual("bbc");
+        It parses_the_subdomain     = () => domain.SubDomain.ShouldEqual("www");
+    }
+
+    [Subject(typeof(Parser))]
+    public class when_given_a_wildcard_url_with_multiple_tld : WithParser {
+        Establish context = () => domain = parser.Parse("http://www.site.nonmetro.tokyo.jp");
+
+        It parses_the_tld           = () => domain.TLD.ShouldEqual("nonmetro.tokyo.jp");
+        It parses_the_maindomain    = () => domain.MainDomain.ShouldEqual("site");
         It parses_the_subdomain     = () => domain.SubDomain.ShouldEqual("www");
     }
 
