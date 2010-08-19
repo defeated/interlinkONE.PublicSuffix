8d7cd78853c1589267dda69d80c23cb7f1f514e0
defeated <defeated2k4@gmail.com>
2010-08-19 07:13:58 -0400

    upgraded docu to latest trunk version, removed platform option from rakefile for now

--- rakefile.rb
+++ rakefile.rb
@@ -2,7 +2,6 @@ require 'albacore'
 
 options = {
   :configuration => "Debug",
-  :platform      => "x86",
   :name          => "PublicSuffix"
 }
 
@@ -12,24 +11,23 @@ desc "Build the solution"
 msbuild :build do |msb|
   msb.targets [:Clean, :Build]
   msb.solution = "src/#{options[:name]}.sln"
-  msb.properties :configuration => options[:configuration],
-                 :platform      => options[:platform]
+  msb.properties :configuration => options[:configuration]
 end
 
 desc "Run the specs"
 mspec :test do |mspec|
 
   # HACK: for some reason, mspec is looking for this in the current working directory
-  cp_r "specs/#{options[:name]}.Specs/bin/#{options[:platform]}/#{options[:configuration]}/data", "data"
+  cp_r "specs/#{options[:name]}.Specs/bin/#{options[:configuration]}/data", "data"
 
   mspec.path_to_command = "tools/Machine.Specifications/mspec.exe"
-  mspec.assemblies "specs/#{options[:name]}.Specs/bin/#{options[:platform]}/#{options[:configuration]}/#{options[:name]}.Specs.dll"
+  mspec.assemblies "specs/#{options[:name]}.Specs/bin/#{options[:configuration]}/#{options[:name]}.Specs.dll"
 end
 
 desc "Generate the documentation"
 docu :docs do |docu|
   docu.path_to_command = "tools/docu/docu.exe"
-  docu.assemblies "src/#{options[:name]}/bin/#{options[:platform]}/#{options[:configuration]}/#{options[:name]}.dll"
+  docu.assemblies "src/#{options[:name]}/bin/#{options[:configuration]}/#{options[:name]}.dll"
 end
 
 desc "Build for Release"
