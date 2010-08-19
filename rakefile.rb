746b8797bb8f3e35627483c8ea7d0e4adbe86427
defeated <defeated2k4@gmail.com>
2010-08-19 12:57:07 -0400

    added tasks to automated build

--- rakefile.rb
+++ rakefile.rb
@@ -2,13 +2,20 @@ require 'albacore'
 
 options = {
   :configuration => "Debug",
-  :name          => "PublicSuffix"
+  :name          => "PublicSuffix",
+  :output        => "build"
 }
 
 task :default => [:build, :test]
 
+desc "Clean the build directory"
+task :clean do
+  rm_rf options[:output]
+  mkdir_p options[:output]
+end
+
 desc "Build the solution"
-msbuild :build do |msb|
+msbuild :build => :clean do |msb|
   msb.targets [:Clean, :Build]
   msb.solution = "src/#{options[:name]}.sln"
   msb.properties :configuration => options[:configuration]
@@ -27,12 +34,24 @@ end
 desc "Generate the documentation"
 docu :docs do |docu|
   docu.path_to_command = "tools/docu/docu.exe"
+  docu.output_location = "docs"
   docu.assemblies "src/#{options[:name]}/bin/#{options[:configuration]}/#{options[:name]}.dll"
 end
 
 desc "Build for Release"
 task :release do
   options[:configuration] = "Release"
-  Rake::Task["build"].invoke
-  Rake::Task["doc"].invoke
+  
+  Rake::Task[:build].invoke
+  Rake::Task[:docs].invoke
+
+  cp_r "src/#{options[:name]}/bin/#{options[:configuration]}", "#{options[:output]}/#{options[:configuration]}"
+  cp_r "docs", "#{options[:output]}/#{options[:configuration]}/docs"
+end
+
+desc "Build zip file for Release"
+zip :dist => :release do |zip|
+  zip.directories_to_zip "#{options[:output]}/#{options[:configuration]}"
+  zip.output_file = "#{options[:name]}-0.0.0.0-#{options[:configuration]}.zip"
+  zip.output_path = "#{options[:output]}"
 end
